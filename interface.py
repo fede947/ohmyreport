c0d0b3c0e0a8581d94579b3fae0f3c32d8940bce
L <lucas@localhost.localdomain>
2019-02-06 16:29:22 -0300

    agrego rango en merge

--- interface.py
+++ interface.py
@@ -7,33 +7,38 @@ def start(vulnerabilities):
     #linkVulnerabilitiesMenu(vulnerabilities)
     exit = False
     while not exit:
-        print()
-        entry = input('ohmyreport>>>').split(" ")
-        command = entry.pop(0)
+        #try:
+            print()
+            entry = input('ohmyreport>>>').split(" ")
+            command = entry.pop(0)
 
-        if command == "show":
-            showVulnerabilities(vulnerabilities)
-            continue
-        if command == "delete":
-            vulnerabilities = delete(vulnerabilities, entry[0])
-            continue
-        if command == "chname":
-            vulnerabilities = changeName(vulnerabilities, entry)
-            continue
-        if command == "merge":
-            vulnerabilities = linkVulnerabilitiesSelection(vulnerabilities, entry[0])
-            continue
-        if command == "help":
-            printHelp()
-            continue
-        if command == "sasha":
-            easterEgg()
-            continue
-        if command == "exit":
-            exit = True
-            continue
-        print('[-] Invalid command ')
-
+            if command == "show":
+                showVulnerabilities(vulnerabilities)
+                continue
+            if command == "delete":
+                vulnerabilities = delete(vulnerabilities, entry[0])
+                continue
+            if command == "chname":
+                vulnerabilities = changeName(vulnerabilities, entry)
+                continue
+            if command == "merge":
+                vulnerabilities = linkVulnerabilitiesSelection(vulnerabilities, entry[0])
+                continue
+            if command == "help":
+                printHelp()
+                continue
+            if command == "sasha":
+                easterEgg()
+                continue
+            if command == "exit":
+                exit = True
+                continue
+            print('[-] Invalid command ')
+        #except KeyboardInterrupt:
+            print()
+            print('[-] nah nah nah')
+        #except:
+            print('[-] Super invalid command (╯°□°）╯︵ ┻━┻')
 
 
 def showVulnerabilities(vulnerabilities):
@@ -42,7 +47,12 @@ def showVulnerabilities(vulnerabilities):
 
 def linkVulnerabilitiesSelection(vulnerabilities, listVuln):
 
-    aux = listVuln.split(",")
+    aux = []
+    if '-' in listVuln:
+        for i in range(int(listVuln[0]),int(listVuln[2]) + 1):
+            aux.append(i)
+    else:
+        aux = listVuln.split(",")
     vulnsNums = [int(i) for i in aux]
     vulnsNums.sort(reverse=True)
     name = vulnerabilities[vulnsNums[-1]].name
